dc136818d5f5aea56c7fa7a290816be98469e02f
Nimratpreet kaur <nimratleo5@gmail.com>
2026-06-12 17:26:59 +0530

    code correction

--- practice15.c
+++ practice15.c
@@ -1,25 +1,26 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    //ques. print sum of first n natural numbers
-   // method 1
-    int i,n;
-    i=1;
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    // ques. print sum of first n natural numbers
+    // method 1
+    int i, n;
+    i = 1;
     printf("Enter a number:");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int sum;
-    sum= n*(n+1)/2;
-    printf("sum is :%d",sum);
-    //method 2 
+    sum = n * (n + 1) / 2;
+    printf("sum is :%d \n", sum);
+    // method 2
     int N;
-    printf("Enter a number:");
-    scanf("%d",&N);
-    int sum_ =0;
-    for(int j = 0 ;j <= N; j++ ){
-        sum =sum_ + j
+    printf("Enter a number: \n");
+    scanf("%d", &N);
+    int sum_ = 0;
+    for (int j = 0; j <= N; j++)
+    {
+        sum_= sum_ + j ;
     }
-    printf("sum is %d"; sum_);
+    printf("sum is %d", sum_);
 
     return 0;
-
 }
