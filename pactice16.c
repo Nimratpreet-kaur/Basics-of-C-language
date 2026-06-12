777034db950a4130946448d4ef8a89655acd9ee9
Nimratpreet kaur <nimratleo5@gmail.com>
2026-06-12 17:48:18 +0530

    another practice problem

---
+++ pactice16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    // print table of number input by the user
+    int n;
+    printf("Enter a number: \n");
+    scanf("%d", &n);
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d", n * i);
+    }
+    return 0,
+}
